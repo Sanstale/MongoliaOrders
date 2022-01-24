4dd8e39d4baf6cddd8375f24de50274b5fcfd019
Sanstale <553098913@qq.com>
2022-01-24 18:14:29 +0800

    改文字

--- static/index.js
+++ static/index.js
@@ -341,11 +341,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
-    return '人？';
+    if (score <= 49) return '舞见';
+    if (score <= 99) return '蒙古上单';
+    if (score <= 149) return '热爱生活';
+    if (score <= 199) return '要来了';
+    return 'NMSL';
 }
 
 function toStr(obj) {
